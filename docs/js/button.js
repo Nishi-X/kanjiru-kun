6d2b4723b0386513eb3d2ace718780ff4bb357f6
Yuma Tsuchida <65499273+hagerondev@users.noreply.github.com>
2022-09-30 08:23:34 +0900

    ver1

--- docs/js/button.js
+++ docs/js/button.js
@@ -1,12 +1,12 @@
-var released = true;
+var released = false;
 var info = {
     "release": {
         "text": "<span><span style='display: inline-block'>Google Playで</span><span style='display: inline-block'>インストール</span></span>",
-        "color": "red",
+        "color": "#F56500",
         "url": "https://play.google.com/store/apps/details?id=com.NishiX.kanjurukun",
     },
     "unrelease": {
-        "text": "ダウンロード準備中",
+        "text": "インストール準備中",
         "color": "gray",
         "url": "#",
     },
@@ -18,5 +18,5 @@ for (let button of buttons) {
     data = info[stateLabel];
     button.innerHTML = data["text"];
     button.setAttribute("href", data["url"]);
-    button.style.backgroundColor = "#F56500";
+    button.style.backgroundColor = data["color"];
 }
